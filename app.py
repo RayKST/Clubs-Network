e584f62cfc58d77341df90cb533d2ac1074c3bf6
Kauan <jailma.kmp@gmail.com>
2022-07-16 20:46:15 -0300

    [feat] new login and main routes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import mysql.connector
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
 
 # Database 
 
@@ -18,6 +18,15 @@ connection = mysql.connector.connect(
 app = Flask(__name__)
 
 #Routes
-@app.route("/login")
-def index():
-    return render_template("login.html")
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form['USERNAME']
+        password = request.form['PASSWORD']
+        return redirect(url_for("main"))
+    else:
+        return render_template("login.html")
+
+@app.route("/main", methods=["GET"])
+def main():
+    return render_template("main.html")
